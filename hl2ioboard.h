b49147efcc281fa5f2e7d22531520294854485ad
James C. Ahlstrom <jahlstr@gmail.com>
2023-07-13 15:15:26 -0400

    Move register names to i2c_registers.h.

--- hl2ioboard.h
+++ hl2ioboard.h
@@ -47,19 +47,6 @@
 #define GPIO27_ADC1	27
 #define GPIO28_ADC2	28
 
-// Register numbers
-#define REG_BUFFER_0		0
-#define REG_BUFFER_1		1
-#define REG_BUFFER_2		2
-#define REG_BUFFER_3		3
-#define REG_FIRMWARE_MAJOR	4
-#define REG_FIRMWARE_MINOR	5
-#define REG_INPUT_PINS		6
-#define REG_RF_INPUTS		11
-#define REG_FAN_SPEED		12
-#define REG_TX_FREQUENCY	13
-#define REG_ANTENNA_TUNER	14
-
 typedef void (*irq_handler)(uint8_t register_number, uint8_t register_datum);
 
 void configure_pins(bool use_uart1, bool use_pwm4a);
--- i2c_registers.h
+++ i2c_registers.h
@@ -1,14 +1,17 @@
 // These are the registers in the HL2 IO board Pico
 
-#define GPIO_DIRECT_BASE	170	// map registers to GPIO pins for direct write
 #define REG_TX_FREQ_BYTE4	0
 #define REG_TX_FREQ_BYTE3	1
 #define REG_TX_FREQ_BYTE2	2
 #define REG_TX_FREQ_BYTE1	3
+
 #define REG_FIRMWARE_MAJOR	4
 #define REG_FIRMWARE_MINOR	5
+
 #define REG_INPUT_PINS		6
+#define REG_ANTENNA_TUNER	7
+#define REG_FAULT		8
+
 #define REG_RF_INPUTS		11
 #define REG_FAN_SPEED		12
-#define REG_TX_FREQUENCY	13
-#define REG_ANTENNA_TUNER	14
+#define REG_TX_FREQ_BYTE0	13
