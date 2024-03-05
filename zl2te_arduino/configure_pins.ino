faaedcf020e6069600424a2b341cfd475f930219
Graeme <gvjury@gmail.com>
2024-03-05 18:54:49 +1300

    Update configure_pins.ino

--- zl2te_arduino/configure_pins.ino
+++ zl2te_arduino/configure_pins.ino
@@ -15,7 +15,9 @@ void configure_pins(bool use_uart1, bool use_pwm4a) {
   // Configure the I2C pins
   Wire1.setSDA(GPIO14_I2C1_SDA);
   Wire1.setSCL(GPIO15_I2C1_SCL);
-
+// i2c_init(i2c1, I2C1_BAUDRATE);
+// i2c_slave_init(i2c1, I2C1_ADDRESS, &i2c_slave_handler);
+  
   // ADC
   adc_init();
   adc_gpio_init(GPIO26_ADC0);
@@ -26,54 +28,34 @@ void configure_pins(bool use_uart1, bool use_pwm4a) {
   gpio_init(GPIO17_In1);
   if (use_uart1) {
     gpio_set_function(GPIO17_In1, GPIO_FUNC_UART);  // UART0 RX
-  } else {
+  }
+  else {
     gpio_set_dir(GPIO17_In1, GPIO_IN);
   }
   gpio_disable_pulls(GPIO17_In1);
-  gpio_init(GPIO18_In2);
-  gpio_set_dir(GPIO18_In2, GPIO_IN);
-  gpio_disable_pulls(GPIO18_In2);
-  gpio_init(GPIO21_In3);
-  gpio_set_dir(GPIO21_In3, GPIO_IN);
-  gpio_disable_pulls(GPIO21_In3);
-  gpio_init(GPIO07_In4);
-  gpio_set_dir(GPIO07_In4, GPIO_IN);
-  gpio_disable_pulls(GPIO07_In4);
-  gpio_init(GPIO06_In5);
-  gpio_set_dir(GPIO06_In5, GPIO_IN);
-  gpio_disable_pulls(GPIO06_In5);
-  gpio_init(GPIO13_EXTTR);
-  gpio_set_dir(GPIO13_EXTTR, GPIO_IN);
-  gpio_disable_pulls(GPIO13_EXTTR);
+  gpio_init(GPIO18_In2);  gpio_set_dir(GPIO18_In2, GPIO_IN);  gpio_disable_pulls(GPIO18_In2);
+  gpio_init(GPIO21_In3);  gpio_set_dir(GPIO21_In3, GPIO_IN);  gpio_disable_pulls(GPIO21_In3);
+  gpio_init(GPIO07_In4);  gpio_set_dir(GPIO07_In4, GPIO_IN);  gpio_disable_pulls(GPIO07_In4);
+  gpio_init(GPIO06_In5);  gpio_set_dir(GPIO06_In5, GPIO_IN);  gpio_disable_pulls(GPIO06_In5);
+  gpio_init(GPIO13_EXTTR);  gpio_set_dir(GPIO13_EXTTR, GPIO_IN);  gpio_disable_pulls(GPIO13_EXTTR);
   gpio_set_irq_enabled_with_callback(GPIO13_EXTTR, 0xc, 1, IrqRxTxChange);  // call on pin change
 
   // configure output pins
-  gpio_init(GPIO25_LED);
-  gpio_set_dir(GPIO25_LED, GPIO_OUT);
-  gpio_put(GPIO25_LED, 0);
-  gpio_init(GPIO00_HPF);
-  gpio_set_dir(GPIO00_HPF, GPIO_OUT);
-  gpio_put(GPIO00_HPF, 0);
-  gpio_init(GPIO01_Sw12);
-  gpio_set_dir(GPIO01_Sw12, GPIO_OUT);
-  gpio_put(GPIO01_Sw12, 0);
-  gpio_init(GPIO02_RF3);
-  gpio_set_dir(GPIO02_RF3, GPIO_OUT);
-  gpio_put(GPIO02_RF3, 0);
-  gpio_init(GPIO03_INTTR);
-  gpio_set_dir(GPIO03_INTTR, GPIO_OUT);
-  gpio_put(GPIO03_INTTR, 0);
+  gpio_init(GPIO25_LED);  gpio_set_dir(GPIO25_LED, GPIO_OUT);  gpio_put(GPIO25_LED, 0);
+  gpio_init(GPIO00_HPF);  gpio_set_dir(GPIO00_HPF, GPIO_OUT);  gpio_put(GPIO00_HPF, 0);
+  gpio_init(GPIO01_Sw12);  gpio_set_dir(GPIO01_Sw12, GPIO_OUT);  gpio_put(GPIO01_Sw12, 0);
+  gpio_init(GPIO02_RF3);  gpio_set_dir(GPIO02_RF3, GPIO_OUT);  gpio_put(GPIO02_RF3, 0);
+  gpio_init(GPIO03_INTTR);  gpio_set_dir(GPIO03_INTTR, GPIO_OUT);  gpio_put(GPIO03_INTTR, 0);
   gpio_init(GPIO04_Fan);		gpio_set_function(GPIO04_Fan,  GPIO_FUNC_PWM); 	// PWM2 A
   pwm_set_wrap(FAN_SLICE, FAN_WRAP);  // configure fan
   pwm_set_chan_level(FAN_SLICE, FAN_CHAN, 0);
   pwm_set_enabled(FAN_SLICE, true);
-  gpio_init(GPIO12_Sw5);
-  gpio_set_dir(GPIO12_Sw5, GPIO_OUT);
-  gpio_put(GPIO12_Sw5, 0);
+  gpio_init(GPIO12_Sw5);  gpio_set_dir(GPIO12_Sw5, GPIO_OUT);  gpio_put(GPIO12_Sw5, 0);
   if (use_uart1) {
     gpio_init(GPIO16_Out1);
     gpio_set_function(GPIO16_Out1, GPIO_FUNC_UART);  // UART0 TX
-  } else {
+  }
+  else {
     gpio_init(GPIO16_Out1);
     gpio_set_dir(GPIO16_Out1, GPIO_OUT);
     gpio_put(GPIO16_Out1, 0);
